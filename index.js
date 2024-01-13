9bea348e9e32dadb1df9472811b604aed152269a
ETEMESI <samweletemesi2@Gmail.com>
2024-01-13 03:21:14 +0300

    Completed the assignment after doing the corrections accordingly.

--- index.js
+++ index.js
@@ -1,39 +1,51 @@
 // Code your solution in this file!
+  
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
 
-const returnFirstTwoDrivers = function(drivers) {
-    return drivers.slice(0, 2);
+function returnFirstTwoDrivers(drivers) {
+  return drivers.slice(0, 2);
+}
+
+function returnLastTwoDrivers(drivers) {
+  return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier) {
+  return function (fare) {
+    return fare * multiplier;
   };
+}
 
-//lastTwoDrivers function
-  const returnLastTwoDrivers = function(drivers) {
-    return drivers.slice(-2);
-  };
-  
-  //Array of the last 2 drivers
-  const driversArray = ['Amari', 'Mo'];
-  const lastTwoDrivers = returnLastTwoDrivers(driversArray);
-  
+const fareDoubler = createFareMultiplier(2);
 
-  const arrayOfFunctions = [returnFirstTwoDrivers, returnLastTwoDrivers];
-  
-  function createFareMultiplier(multiplier) {
-    
-    return function(fare) {
-      return fare * multiplier;
-    };
-  }
-  
-  const fareDoubler = createFareMultiplier(2);
-  const fareTripler = createFareMultiplier(3);
-  
-  // Applying the created functions to calculate fares
-  const originalFare = 40;
-  
-  const fareDoubled = fareDoubler(originalFare);
-  const fareTripled = fareTripler(originalFare);
+const fareTripler = createFareMultiplier(3);
 
-  
-  console.log(fareDoubled);
-  console.log(fareTripled);    
-  
- 
+function selectDifferentDrivers(drivers, fn) {
+  return fn(drivers);
+}
+
+//  returnFirstTwoDrivers
+console.log(returnFirstTwoDrivers(drivers)); // ['Antonia', 'Nuru']
+
+// returnLastTwoDrivers
+console.log(returnLastTwoDrivers(drivers)); // ['Amari', 'Mo']
+
+// selectingDrivers
+console.log(selectingDrivers[0](drivers)); // ['Antonia', 'Nuru']
+console.log(selectingDrivers[1](drivers)); // ['Amari', 'Mo']
+
+// createFareMultiplier
+const fareQuintupler = createFareMultiplier(5);
+console.log(fareQuintupler(5)); // 25
+
+// fareDoubler
+console.log(fareDoubler(10)); // 20
+
+// fareTripler
+console.log(fareTripler(12)); // 36
+
+// selectDifferentDrivers
+console.log(selectDifferentDrivers(drivers, returnFirstTwoDrivers)); // ['Antonia', 'Nuru']
+console.log(selectDifferentDrivers(drivers, returnLastTwoDrivers)); // ['Amari', 'Mo']
